56d3757b4d361fd1312bc8c9ac8b81279abbe2b9
michael <michael@cadot.eu>
2024-06-24 20:05:35 +0200

    add vérification pid

--- rcloneSync.sh
+++ rcloneSync.sh
@@ -6,7 +6,7 @@ REMOTE_DIR="gdrive:cloud"
 
 BASEDIR=$(dirname "$0")
 # Fichier temporaire pour les logs de rclone
-LOG_FILE=${BASEDIR}/rclone_logs.txt
+LOG_FILE="${BASEDIR}/rclone_logs.txt"
 
 xfconf-query -c xfce4-notifyd -p /notification-log -s true
 
@@ -41,6 +41,14 @@ sync() {
 
     echo "Synchronisation avec rclone terminée."
 
+    # Vérification de la phrase "There was nothing to transfer" dans les logs
+    count=$(grep -o "There was nothing to transfer" "$LOG_FILE" | wc -l)
+
+    if [ "$count" -eq 2 ]; then
+        echo "Rien à faire. Fin du script."
+        exit 0
+    fi
+
     # Filtrer les logs pour supprimer les lignes indésirables
     filtered_logs=$(grep -v -E '^Transferred:|^Checks:|^Deleted:|^Elapsed time:|^.*There was nothing to transfer$' "$LOG_FILE" | grep -v -E '^.*INFO\s*:\s*$')
 
@@ -52,12 +60,37 @@ sync() {
     if [ -n "$filtered_logs" ]; then
         notify-send "Synchronisation rclone" "$filtered_logs"
     fi
-
 }
-# on lance pas le sync si on a un processus en cours qui s'appele rcloneSync.sh
-if ! pgrep -x "rcloneSync.sh" > /dev/null
-then
+
+# Fonction pour vérifier si le processus est en cours d'exécution
+is_process_running() {
+    local pid=$1
+    if ps -p "$pid" > /dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+script_name=$(basename "$0")
+
+# Vérifie si le processus est déjà en cours d'exécution
+pids=$(pgrep -f "$script_name")
+
+# Filtrer pour exclure le PID de ce script en cours d'exécution
+current_pid=$$
+pids=$(echo "$pids" | grep -vw "$current_pid")
+
+if [ -n "$pids" ]; then
+    for pid in $pids; do
+        if is_process_running "$pid"; then
+            echo "Le processus $script_name est en cours d'exécution avec PID $pid."
+            echo "$script_name est en cours d'exécution. Merci de patienter..."
+            exit 1
+        fi
+    done
+else
+    echo "Aucun processus $script_name trouvé. Lancement de la synchronisation."
     sync
-else    
-    echo "rcloneSync.sh est en cours d'exécution. Merci de patienter..."
 fi
+
